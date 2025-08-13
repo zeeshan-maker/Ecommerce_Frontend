5b96077e59fea143d3fd0cb4c6215f66c3b0ff1c
zeeshan-techtorch <zeeshan.khan@techtorch.solutions>
2025-08-13 19:57:04 +0530

    add popular section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,11 @@
-import "./Home.css";
 import Hero from "../../components/Hero/Hero";
-import Card from "../../components/Card/Card";
-import all_product from "../../assets/Frontend_Assets/all_product";
+import Popular from "../../components/Popular/Popular";
 function Home() {
-  const womens_product = all_product.filter((product)=> product.category === "women")
+
   return (
     <>
       <Hero />
-      <div className="container py-lg-4 py-md-3 py-2">
-        <div className="row">
-          <h1 className="text-center">POPULAR IN WOMEN</h1>
-          <hr className="underline" />
-        </div>
-        <div className="row">
-          {
-          womens_product.slice(0, 4).map((product) => (
-              <div key={product.id} className="col-lg-3 col-md-4 col-sm-6 mb-3">
-              <Card 
-              product_id={product.id} 
-              image={product.image}
-              name={product.name}
-              price={product.new_price}
-              />
-            </div>
-           
-          )
-        )
-          }
-        </div>
-      </div>
+      <Popular/> 
     </>
   );
 }
