03e2f7eb24e6cf5acf8abaa4b5f0f60ea2ea4a66
zeeshan-techtorch <zeeshan.khan@techtorch.solutions>
2025-09-14 08:04:54 +0530

    add backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://ecommerce-backend-735s.onrender.com/api/v1",
 });
 
 //  Add token automatically for every request
