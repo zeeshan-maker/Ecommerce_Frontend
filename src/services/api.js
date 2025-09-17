94bc298d80325902358e88bc8d38b00d6e1f4c0c
zeeshan-techtorch <zeeshan.khan@techtorch.solutions>
2025-09-17 20:06:36 +0530

    change url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://ecommerce-backend-735s.onrender.com/api/v1",
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://ecommerce-backend-735s.onrender.com/api/v1",
+  // baseURL: "http://localhost:4000/api/v1",
 });
 
 //  Add token automatically for every request
