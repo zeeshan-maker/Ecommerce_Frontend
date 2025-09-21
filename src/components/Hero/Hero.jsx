e6f2288f85fcdefb3f760ead04db9f7368589369
zeeshan-techtorch <zeeshan.khan@techtorch.solutions>
2025-09-21 19:54:16 +0530

    add blure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,8 @@ import arrow from "../../assets/Frontend_Assets/arrow.png"
 import "./Hero.css"
 function Hero() {
   return (
-    <div className="container-fluid bg-color">
-        <div className="container py-lg-5">
+    <div className="container-fluid">
+        <div className="container py-lg-5 py-5">
         <div className="row">
            <div className="col-lg-6 d-flex align-items-center justify-content-center order-2 order-sm-1">
              <div className="w-100">
@@ -21,12 +21,13 @@ function Hero() {
              </div>
            </div>
 
-           <div className="col-lg-6 d-flex align-items-center justify-content-center order-1 order-sm-2 ">
-                <img src={hero_image} className="img-fluid h-lg-75 h-md-75" alt="" />
+           <div className="col-lg-6 d-flex align-items-center justify-content-center order-1 order-sm-2 position-relative">
+                <img src={hero_image} className="img-fluid" alt="" />
+                <div className="background"></div>
            </div>
         </div>
 
-    </div>
+         </div>
     </div>
   )
 }
